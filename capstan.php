950c1ed28429317e0f7cddde804a5f311ae7f3d6
Benedict <ben@benedict-wallis.com>
2026-02-20 16:24:46 +0000

    Remove 'capstan make' and 'capstan theme' commands
    Fixes: Uncaught Exception: 'wp capstan make' can't have subcommands. Unregister the top-level WP-CLI commands 'capstan make' and 'capstan theme' from capstan.php. The file now only registers the subcommands 'capstan make child' and 'capstan theme package'.

--- capstan.php
+++ capstan.php
@@ -6,7 +6,5 @@ if (! defined('WP_CLI') || ! WP_CLI) {
 
 WP_CLI::add_command('capstan about', \PressGang\Capstan\Commands\AboutCommand::class);
 WP_CLI::add_command('capstan new', \PressGang\Capstan\Commands\NewCommand::class);
-WP_CLI::add_command('capstan make', \PressGang\Capstan\Commands\MakeCommand::class);
 WP_CLI::add_command('capstan make child', \PressGang\Capstan\Commands\MakeChildCommand::class);
-WP_CLI::add_command('capstan theme', \PressGang\Capstan\Commands\ThemeCommand::class);
 WP_CLI::add_command('capstan theme package', \PressGang\Capstan\Commands\ThemePackageCommand::class);
